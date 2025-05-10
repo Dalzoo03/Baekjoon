26381623ed1766eb0142fb6b3a67d8aa7e0b6c8b
Dalzoo_ <102957371+Dalzoo03@users.noreply.github.com>
2025-05-10 16:30:55 +0900

    [Bronze I] Title: 이항 계수 1, Time: 180 ms, Memory: 17704 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main{
+  public static void main(String args[]){
+    Scanner scanner = new Scanner(System.in);
+    int N = scanner.nextInt();
+    int K = scanner.nextInt();
+
+    int upper = 1;
+    for(int i = 1; i <= N; i++){
+      upper *= i;
+    }
+    int lower1 = 1;
+    for(int i = 1; i <= K; i++){
+      lower1 *= i;
+    }
+    int lower2 = 1;
+    for(int i = 1; i <= N-K; i++){
+      lower2 *= i;
+    }
+    System.out.println(upper/(lower1 * lower2));
+    
+  }
+}
